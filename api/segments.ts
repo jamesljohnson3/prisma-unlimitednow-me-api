54c43c35b91f75ca8cfb23439cf17d2f0eef9b72
Admin <support@unlimitpotential.com>
2024-09-24 08:40:31 -0400

    j

--- api/segments.ts
+++ api/segments.ts
@@ -1,16 +1,10 @@
 import { PrismaClient, Prisma } from '@prisma/client'
 import { VercelRequest, VercelResponse } from '@vercel/node'
 
-type VercelRequestQuery = {
-  userId?: string
-}
-
 const prisma = new PrismaClient()
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   try {
-    const { userId } = req.query as VercelRequestQuery
-
     console.log(
       '[account] Incoming request:',
       JSON.stringify(
@@ -26,31 +20,22 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
 
     switch (req.method) {
       case 'GET':
-        if (userId) {
-          // Fetch segments associated with the user
-          const segments = await prisma.segment.findMany({
-            where: {
-              users: {
-                some: {
-                  id: userId,
-                },
-              },
-            },
-            include: {
-              users: true, // Include associated users
-              domain: true, // Include associated domain
-              // Add more related models here if needed
-            },
-          })
-          return res.json(segments)
-        } else {
-          return res.status(400).json({ message: 'Missing userId for fetching segments' })
-        }
+        // Fetch all segments
+        const segments = await prisma.segment.findMany({
+          include: {
+            users: true,  // Include associated users
+            domain: true, // Include associated domain
+            // Add more related models here if needed
+          },
+        })
+        return res.json(segments)
+
       case 'POST':
         const createdSegment = await prisma.segment.create({
           data: req.body as Prisma.SegmentCreateInput,
         })
         return res.status(201).json(createdSegment)
+
       case 'PUT':
         const { id } = req.body
         if (id) {
@@ -62,6 +47,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
         } else {
           return res.status(400).json({ message: 'Missing id for updating segment' })
         }
+
       case 'DELETE':
         const deleteId = req.body.id
         if (deleteId) {
@@ -72,6 +58,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
         } else {
           return res.status(400).json({ message: 'Missing id for deleting segment' })
         }
+
       default:
         return res.status(405).json({ message: `Method ${req.method} not allowed` })
     }
